0ace756994eb368d9dcde66d4f4a66bd90246d6b
Farha B. Jawed <farhajw@gmail.com>
2018-06-01 03:58:50 -0700

    added tags

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,28 @@
-function searchBandsInTown(artist) {
+
+function generateTags(){
     var apiKeyLastFm ="c5170f76db40cf305fa1f7989ee80687";
+    queryURLTags = "http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key="+apiKeyLastFm+"&format=json";
+    $.ajax({
+        url: queryURLTags,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        var tagArray = response.toptags.tag;
+        for(var i = 0; i < 10; i++){
+            console.log(tagArray[i].name);
+            var tagBtn = $("<button>").text(tagArray[i].name);
+            $("#tag").append(tagBtn);
+        }
+      });
+}
 
-    var queryURLArtist = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist + "&api_key="+apiKeyLastFm+"&format=json";
 
+// generateTags();
+
+function searchBandsInTown(artist) {
+    var apiKeyBandInTown = "codingbootcamp";
+    var queryURLArtist  = "https://rest.bandsintown.com/artists/" + artist + "?app_id="+apiKeyBandInTown;
+   
     //1st api call
     $.ajax({
       url: queryURLArtist,
@@ -17,85 +37,71 @@ function searchBandsInTown(artist) {
       $("#lyric-div").empty();
        console.log(queryURLArtist);
        console.log(response);
-
-       var artistArray = response.results.artistmatches.artist;
-       for(var i = 0; i< artistArray.length; i++){
-           var artistName = artistArray[i].name;
-           var artistImageUrl = artistArray[i].image[2]["#text"];
-           var mbid = artistArray[i].mbid;
-           var listeners = artistArray[i].listeners;
-           if(artistName.toLowerCase() === artist.toLowerCase()){
-             $("#artist-div").addClass("artist-div");
-              var artistDiv = $("<div>");
-              artistDiv.attr("data-mbid",mbid);
-              var imageDiv = $("<div>");
-              var artistImage = $("<img>").attr("src",artistImageUrl);
-              var name = $("<h1>").text(artistName);
-              var listenersP = $("<p>").text("Listeners: "+listeners);
-              imageDiv.append(artistImage);
-              artistDiv.append(imageDiv, name);
-
-             
-              console.log(mbid);
-              queryURLArtist = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+artist+"&mbid="+mbid+"&api_key="+apiKeyLastFm+"&format=json";
-              console.log("2nd: "+queryURLArtist);      
-              
-              //2nd api call
-               $.ajax({
-                   url: queryURLArtist,
-                   method: "GET"
-               }).then(function(response) {
-                   console.log(response);
-                   var artistBio = response.artist.bio.summary;
-                   var bioP = $("<p>").html("Bio:"+artistBio);
-                   artistDiv.append(bioP,listenersP);
-                   $("#artist-div").append(artistDiv);    
-   
-               });
-
-               
-               queryURLArtist = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+artist+"&mbid="+mbid+"&api_key="+apiKeyLastFm+"&format=json";
-               console.log("3rd: "+queryURLArtist); 
-               //3rd api call
-               $.ajax({
-                   url: queryURLArtist,
-                   method: "GET"
-               }).then(function(response) {
-                   console.log(response);
-                    var trackArray = response.toptracks.track;
-                    $("#top-track-div").html("<h1>Top 10 Tracks</h1>")
-                    $("#top-track-div").addClass("track-div");
-                    for(var i = 0; i<10;i++){
-                        console.log(trackArray[i].name);
-                        var track = trackArray[i].name;
-                       // var playCount = trackArray[i].playcount;
-                        var trackDiv = $("<div>");
-                        trackDiv.addClass("track")
-                        trackDiv.attr("data-artist",artistName);
-                        trackDiv.html(track);
-                        $("#top-track-div").append(trackDiv);
-
-                    }                
-               });
-              
-               break;
-           }     
-       }
       
+       var artistName = response.name;
+       var mbid = response.mbid;
+       var trackers = response.tracker_count;
+       $("#artist-div").addClass("artist-div");
+       var artistDiv = $("<div>");
+       var name = $("<h1>").text(artistName);
+       var trackersP = $("<p>").text("Trackers: "+trackers);
+                  
+       
+        var apiKeyLastFm ="c5170f76db40cf305fa1f7989ee80687";
+        queryURLArtist = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+artistName+"&mbid="+mbid+"&api_key="+apiKeyLastFm+"&format=json";
+        console.log("2nd: "+queryURLArtist);      
+        
+        //2nd api call
+        $.ajax({
+            url: queryURLArtist,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var artistBio = response.artist.bio.summary;
+            var artistImageUrl = response.artist.image[3]["#text"];
+            var imageDiv = $("<div>");
+            var artistImage = $("<img>").attr("src",artistImageUrl);
+            imageDiv.append(artistImage);
+            var bioP = $("<p>").html("Bio:"+artistBio);
+            artistDiv.append(imageDiv, name,bioP,trackersP);
+            $("#artist-div").append(artistDiv);    
+
+            queryURLArtist = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+artistName+"&mbid="+mbid+"&api_key="+apiKeyLastFm+"&format=json";
+            console.log("3rd: "+queryURLArtist); 
+    
+            //3rd api call
+            $.ajax({
+                url: queryURLArtist,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+                var trackArray = response.toptracks.track;
+                $("#top-track-div").html("<h1>Top 10 Tracks</h1>")
+                $("#top-track-div").addClass("track-div");
+                for(var i = 0; i<10;i++){
+                    console.log(trackArray[i].name);
+                    var track = trackArray[i].name;
+                    // var playCount = trackArray[i].playcount;
+                    var trackDiv = $("<div>");
+                    trackDiv.addClass("track")
+                    trackDiv.attr("data-artist",artistName);
+                    trackDiv.html(track);
+                    $("#top-track-div").append(trackDiv);
+                }                
+            });
+            // http://ws.audioscrobbler.com/2.0/?method=track.search&track=i%20choose%20you&artist=sara%20bareilles&api_key=c5170f76db40cf305fa1f7989ee80687&format=json
+
+        });   
     });
   }
 
   $("#select-artist").on("click", function(event) {
-   
-    event.preventDefault();
-    
-    var inputArtist = $("#artist-input").val().trim();
-
-    
+    event.preventDefault(); 
+    var inputArtist = $("#artist-input").val().trim();  
     searchBandsInTown(inputArtist);
   });
 
-
+  
   $("#top-track-div").on("click", ".track",function(event) {
     $("#lyric-div").empty();
     var inputTrack = $(this).text();
@@ -111,6 +117,10 @@ function searchBandsInTown(artist) {
         var lyric = response.result.track.text.replace(/(?:\r\n|\r|\n)/g, '<br>');
         var inputTrackHtml = $("<h1>").text("Lyric: "+inputTrack);
         $("#lyric-div").append(inputTrackHtml, JSON.stringify(lyric));         
+    }).fail(function() {
+        console.log("error");
+        var inputTrackHtml = $("<h1>").text("Lyric: "+inputTrack);
+        $("#lyric-div").append(inputTrackHtml, "Lyric Not Found"); 
     });
 
   });
