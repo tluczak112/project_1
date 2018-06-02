65561db4074f93b2fc5bfe64de0d13a3807760c5
Farha B. Jawed <farhajw@gmail.com>
2018-06-02 13:19:02 -0700

    draft version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,6 +40,7 @@ function searchBandsInTown(artist) {
       $("#lyric-div").removeClass();
       $("#lyric-div").empty();
       $("#genre-div").empty();
+      $("#video-div").empty();
        console.log(queryURLArtist);
        console.log(response);
       
@@ -79,25 +80,46 @@ function searchBandsInTown(artist) {
                 url: queryURLArtist,
                 method: "GET"
             }).then(function(response) {
-                console.log(response);
+               // console.log(response);
                 var trackArray = response.toptracks.track;
-                $("#top-track-div").html("<h1>Top 10 Tracks</h1>")
+                $("#top-track-div").html("<h1>Top Tracks</h1>")
                 $("#top-track-div").addClass("track-div");
+                var tracks = [];
                 for(var i = 0; i<10;i++){
-                    console.log(trackArray[i].name);
+                    console.log("*"+trackArray[i].name);
                     var track = trackArray[i].name;
-                    var trackDiv = $("<div>");
-                    trackDiv.addClass("track")
-                    trackDiv.attr("data-artist",artistName);
-                    trackDiv.html(track);
-                    $("#top-track-div").append(trackDiv);
-                }                
+                    tracks.push(track);
+                    playVideo(track,artistName);
+                }
             });
-            // http://ws.audioscrobbler.com/2.0/?method=track.search&track=i%20choose%20you&artist=sara%20bareilles&api_key=c5170f76db40cf305fa1f7989ee80687&format=json
-
         });   
     });
   }
+
+  function playVideo(track,artistName){
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+track+" by "+artistName+"&type=video&key=AIzaSyDxxuZIfzfQ8B49df69ZPTapVglg6HJd5Q";
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var title = response.items[0].snippet.title;
+        var vid = response.items[0].id.videoId;
+        var trackDiv = $("<div>");
+        trackDiv.addClass("track")
+        trackDiv.attr("data-artist",artistName);
+        trackDiv.attr("data-title",title);
+        trackDiv.attr("data-vid",vid);
+        trackDiv.html(title);
+        // var link = $("<a target='_blank'>");
+        // link.attr("href","https://www.youtube.com/watch?v="+vid);
+        // link.text(track);
+       // trackDiv.append(link);
+        $("#top-track-div").append(trackDiv);
+    });
+  }
+
 
   $("#select-artist").on("click", function(event) {
     event.preventDefault(); 
@@ -105,15 +127,28 @@ function searchBandsInTown(artist) {
     searchBandsInTown(inputArtist);
   });
 
-  
+  var players = [];
+
+
   $("#top-track-div").on("click", ".track",function(event) {
     $("#lyric-div").empty();
+    $("#video-div").empty();
     var inputTrack = $(this).text();
     var artist = $(this).attr("data-artist");
+    var vid = $(this).attr("data-vid");
     var apiKey = "S0unXXfz6D5EzyIM6971qYOyXZa96p0TB8WIPPxH1NCAPNT17QMNVPlrWFEMg6mL";
     $("#lyric-div").addClass("lyric");
     var queryURLLyric = "https://orion.apiseeds.com/api/music/lyric/"+artist+"/"+inputTrack+"?apikey="+apiKey;
     console.log(queryURLLyric);
+    var blk = $("<blockquote class='embedly-card'>");
+    var link = $("<a>");
+    link.attr("href","https://www.youtube.com/watch?v="+vid+"?autoplay=1&cc_load_policy=1&loop=1");
+    var linkH = $("<h4>");
+    linkH.append(link);
+    blk.append(linkH,$("<p>"));
+    $("#video-div").append(blk);
+   
+   
     $.ajax({
         url: queryURLLyric,
         method: "GET"
