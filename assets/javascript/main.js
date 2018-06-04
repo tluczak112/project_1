675e3e70f1e0febce3f36966b58851a85a48eb74
Phazeon <tjl82996@gamil.com>
2018-06-04 12:21:44 -0700

    Updated Logo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,9 @@
-// $(".main-page").hide();
-// var timerId = setTimeout(myTimer, 4000);
+$(".main-page").hide();
+var timerId = setTimeout(myTimer, 4000);
 
 function myTimer(){
-    $("body").removeClass("main");
-    $("body").addClass("main2");
+    // $("body").removeClass("main");
+    // $("body").addClass("main2");
     $(".wrapper").remove();
      $(".main-page").show();
 }
