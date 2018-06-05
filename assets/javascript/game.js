707380403044eddc33b03e01125b96e8c0ebdf6e
Farha Jawed <farhajw@gmail.com>
2018-06-05 14:11:21 -0700

    error message

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,10 +32,10 @@ $(document).ready(function () {
             url: queryURLGenre,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
             var genreArray = response.toptags.tag;
             for (var i = 0; i < 10; i++) {
-                console.log(genreArray[i].name);
+                // console.log(genreArray[i].name);
                 var genre = genreArray[i].name;
                 var genreLink = $("<a>").text(genre);
                 genreLink.addClass("waves-effect waves-light btn-small cyan darken-2 tag");
@@ -51,7 +51,7 @@ $(document).ready(function () {
         i = 0;
         var apiKeyBandInTown = "codingbootcamp";
         var queryURLArtist = "https://rest.bandsintown.com/artists/" + artist + "?app_id=" + apiKeyBandInTown;
-        console.log(queryURLArtist);
+        // console.log(queryURLArtist);
         artistTrackDiv.empty();
         playDiv.hide();
         nowplayDiv.empty();
@@ -66,7 +66,7 @@ $(document).ready(function () {
             url: queryURLArtist,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
             var artistName = response.name;
             var mbid = response.mbid;
             var trackers = response.tracker_count;
@@ -81,7 +81,7 @@ $(document).ready(function () {
                 url: queryURLArtist,
                 method: "GET"
             }).then(function (response) {
-                console.log(response);
+                // console.log(response);
                 var artistBio = response.artist.bio.summary;
                 var artistImageUrl = response.artist.image[3]["#text"];
                 var artistImageHtml = $("#artist-image").attr("src", artistImageUrl);
@@ -98,23 +98,21 @@ $(document).ready(function () {
                 artistDetailDiv.show();
 
                 var queryURLTrack = "https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artistName + "&mbid=" + mbid + "&api_key=" + apiKeyLastFm + "&format=json";
-                console.log("3rd: " + queryURLTrack);
+                // console.log("3rd: " + queryURLTrack);
 
                 //3rd api call
                 $.ajax({
                     url: queryURLTrack,
                     method: "GET"
                 }).then(function (response) {
-                    console.log(response);
+                    // console.log(response);
                     var trackArray = response.toptracks.track;
                     var tracks = [];
                     for (var i = 0; i < 10; i++) {
-                        console.log("*" + trackArray[i].name);
+                        // console.log("*" + trackArray[i].name);
                         var track = trackArray[i].name;
                         tracks.push(track);
                         getVideo(track, artistName);
-
-
                     }
 
                     //4th api call
@@ -173,7 +171,7 @@ $(document).ready(function () {
                 });//3rd api
             });//2nd api
         }).fail(function(){
-            console.log("error");
+            // console.log("error");
             artistDetailDiv.hide();
             trackDiv.hide();
             playDiv.hide();
@@ -184,12 +182,12 @@ $(document).ready(function () {
 
     function getVideo(track, artistName) {
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + track + " by " + artistName + "&type=video&key=AIzaSyDxxuZIfzfQ8B49df69ZPTapVglg6HJd5Q";
-        console.log(queryURL);
+        // console.log(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
             var youtubeTitle = response.items[0].snippet.title;
             var vid = response.items[0].id.videoId;
             var icon = $("<i>").addClass("material-icons right play").html("play_circle_outline");
@@ -227,14 +225,10 @@ $(document).ready(function () {
         var icon = $(this).children();
         icon.removeClass("play").addClass("now-play").html("play_circle_filled");
         $(this).addClass("red-text");
-
         getLyrics(artist, inputTrack);
-
-
     });
 
     //lyrics function
-    
     function getLyrics(artist, inputTrack) {
         var apiLyrics = "3ovTtzy3bh4BgFmV33tZ1xoRY5DbXgj7azJKfxROe8b6kbMhc8tIWBPnP5dHDypJ";
         var queryUrlLyrics = "https://orion.apiseeds.com/api/music/lyric/" + artist + "/" + inputTrack + "?apikey=" + apiLyrics;
@@ -243,7 +237,7 @@ $(document).ready(function () {
             url: queryUrlLyrics,
             method: "GET"
         }).then(function (response) {
-
+            $(".lyric-title").empty();
             var lyric = response.result.track.text.replace(/(?:\r\n|\r|\n)/g, '<br>');
             var inputTrackHtml = $("<h1>").text(artist + " : " + inputTrack);
             $(".lyrics-title").html(inputTrackHtml);
@@ -252,10 +246,10 @@ $(document).ready(function () {
             lyricsDiv.show();
 
         }).fail(function () {
-            console.log("error");
-            var inputTrackHtml = $("<h1>").text("Lyric: " + inputTrack);
-            $(".lyrics-title").html(inputTrackHtml);
-            $(".show-lyrics").html(inputTrackHtml, "Lyric Not Found");
+            console.log("***error");
+            $(".lyric-title").empty();
+            $(".show-lyrics").empty();
+            $(".lyrics-title").html("<h1>Lyric: " + inputTrack+"</h1><br><span class='message'>Lyric Not Found :(</span>");
             lyricsDiv.show();
         });
 
@@ -289,16 +283,16 @@ $(document).ready(function () {
     
     function linkToYoutube(track,artistName){
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+track+" by "+artistName+"&type=video&key=AIzaSyDxxuZIfzfQ8B49df69ZPTapVglg6HJd5Q";
-        console.log(queryURL);
+        // console.log(queryURL);
         $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-            console.log(response);
+            // console.log(response);
             var youtubeTitle = response.items[0].snippet.title;
             var vid = response.items[0].id.videoId;
             var thumbnail = response.items[0].snippet.thumbnails.high.url;
-            console.log(youtubeTitle, thumbnail, vid);
+            // console.log(youtubeTitle, thumbnail, vid);
             var listDiv = $("<li>").addClass("collection-item avatar grey darken-4 left-align list-div");
             var thumbnailImage = $("<img>").attr("src",thumbnail);
             thumbnailImage.addClass("circle artist-thumbnail responsive-img");
@@ -322,7 +316,7 @@ $(document).ready(function () {
 
      //click on genre tag
       $("#genre").on("click", ".tag", function(event) {
-        console.log("hello");
+        // console.log("hello");
         $("#genre-track").empty();
         bottomDiv.empty();
         clearSelectedGenre();
@@ -335,17 +329,17 @@ $(document).ready(function () {
             url: queryURLTag,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
+            // console.log(response);
             var toptrackArrayByGenre = response.tracks.track;
             $("#genre-title").html("Top Tracks: "+genre);
             for(var i = 0; i < 10; i++){
                   var track = toptrackArrayByGenre[i].name;
                   var artistName = toptrackArrayByGenre[i].artist.name;
-                  console.log(track, artistName);   
+                //   console.log(track, artistName);   
                   linkToYoutube(track, artistName); 
             }
         }).fail(function() {
-            console.log("error");
+            // console.log("error");
         });
       });
       
@@ -380,7 +374,7 @@ $(document).ready(function () {
 
     $("#genre-track").on("click", ".artist-icon", function(event) {
        var artist = $(this).attr("data-name");
-       console.log(artist);
+    //    console.log(artist);
        searchBandsInTown(artist);
     });
 
@@ -389,10 +383,10 @@ $(document).ready(function () {
     $(".favt").on("click",function(event){
         var favArtist = $(".favt").attr("data-fav");
         var favArtistImage = $(".favt").attr("data-favImg");
-        console.log(favArtist,favArtistImage);
+        // console.log(favArtist,favArtistImage);
         //favt not found...add to fav!
         if(findObjectByKey(favorites, 'favArtist', favArtist)===-1){
-            console.log("added to fav!");
+            // console.log("added to fav!");
             addToFavorite();
             favorites.push({favArtist,favArtistImage});
             localStorage.setItem("favorites", JSON.stringify(favorites));
@@ -401,11 +395,11 @@ $(document).ready(function () {
         else{
             removeFromFavorite();
             var favIndex = findObjectByKey(favorites, 'favArtist', favArtist);
-            console.log(favIndex);
+            // console.log(favIndex);
             favorites.splice(favIndex,1);
             localStorage.setItem("favorites", JSON.stringify(favorites));
         }
-        console.log(favorites);
+        // console.log(favorites);
     });
 
     function addToFavorite(){
@@ -440,7 +434,7 @@ $(document).ready(function () {
 
    //show favorites
    $(".fav-link").on("click",function(event){
-       console.log(favorites);
+    //    console.log(favorites);
        hideAll();
        genreDiv.hide();
        clearSelectedGenre();
@@ -460,7 +454,7 @@ $(document).ready(function () {
        else{
         for(var i = 0; i<favorites.length;i++){
             var container = $("<div>").addClass("col s12 m2 fav-artist");
-            console.log(favorites[i].favArtistImage);
+            // console.log(favorites[i].favArtistImage);
             var img = $("<img>").attr("src",favorites[i]["favArtistImage"]);
             img.addClass("responsive-img circle fav-artist-img");
             img.attr("data-artistname",favorites[i]["favArtist"]);
